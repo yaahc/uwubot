6eec51921b657b7f48a17d1c41544beae721f524
Jane Lusby <jlusby@yaah.dev>
2021-03-27 12:44:24 -0700

    split config to be separate from structopt arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 use color_eyre::eyre;
+use structopt::StructOpt;
+use uwubot::{Bot, Config};
+
+#[derive(Debug, StructOpt)]
+struct Args {
+    /// Discord Bot's token
+    bot_token: String,
+
+    /// Discord Bot Client ID to register slash commands for
+    #[structopt(short, long)]
+    client_id: u64,
+
+    /// Discord guild ID to register slash commands for
+    #[structopt(short, long)]
+    guild_id: Option<u64>,
+}
+
+impl From<Args> for Config {
+    fn from(args: Args) -> Self {
+        let Args {
+            bot_token,
+            guild_id,
+            client_id,
+        } = args;
+
+        Self {
+            bot_token,
+            guild_id,
+            client_id,
+        }
+    }
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
 
-    let bot = uwubot::Bot::new();
+    let config = Args::from_args();
+    let bot = Bot::new(config);
     bot.run()?;
 
     Ok(())
